121cc35d1c46201b501d1025880f8ddc7ed61ea9
Lena Kiselova <154414482+accidental7Phosphorus@users.noreply.github.com>
2024-03-23 15:47:05 +0100

    Delete src directory

--- src/Ellipse.java
+++
@@ -1,9 +0,0 @@
-public class Ellipse extends Shape{
-    private Point center;
-    private Style style;
-    private float radius;
-    @Override
-    public String toSvg() {
-        return "<ellipse rx=\"" + radius + "\" ry=\"" + radius + "\" cx=\"" + center.x + "\" cy=\"" + center.y + "\" " + style.toSvg();
-    }
-}
